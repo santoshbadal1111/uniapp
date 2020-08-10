669f186378fc13fee1827510e1397833c3e8ac1c
Tushar Desai <tdesai@Tushars-MacBook-Air-4.local>
2020-08-10 14:29:33 +0530

    uniapp first commit

--- src/app/pages/session-detail/session-detail.ts
+++ src/app/pages/session-detail/session-detail.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { ConferenceData } from '../../providers/conference-data';
 import { ActivatedRoute } from '@angular/router';
 import { UserData } from '../../providers/user-data';
+import { PreviewAnyFile } from '@ionic-native/preview-any-file/ngx';
+/* declare var RazorpayCheckout:any; */
 
 @Component({
   selector: 'page-session-detail',
@@ -17,7 +19,8 @@ export class SessionDetailPage {
   constructor(
     private dataProvider: ConferenceData,
     private userProvider: UserData,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private previewAnyFile: PreviewAnyFile
   ) { }
 
   ionViewWillEnter() {
@@ -49,6 +52,9 @@ export class SessionDetailPage {
 
   sessionClick(item: string) {
     console.log('Clicked', item);
+    this.previewAnyFile.preview("http://www.africau.edu/images/default/sample.pdf")
+    .then((res: any) => console.log(res))
+    .catch((error: any) => console.error(error));
   }
 
   toggleFavorite() {
@@ -63,5 +69,31 @@ export class SessionDetailPage {
 
   shareSession() {
     console.log('Clicked share session');
+    /* var options = {
+      description: 'Credits towards consultation',
+      image: 'https://i.imgur.com/3g7nmJC.png',
+      currency: 'INR',
+      key: 'rzp_test_1DP5mmOlF5G5ag',
+      order_id: 'order_7HtFNLS98dSj8x',
+      amount: '5000',
+      name: 'foo',
+      theme: {
+        color: '#F37254'
+      }
+    }
+    
+    var successCallback = function(success) {
+      alert('payment_id: ' + success.razorpay_payment_id)
+      var orderId = success.razorpay_order_id
+      var signature = success.razorpay_signature
+    }
+    
+    var cancelCallback = function(error) {
+      alert(error.description + ' (Error '+error.code+')')
+    }
+    
+    RazorpayCheckout.on('payment.success', successCallback)
+    RazorpayCheckout.on('payment.cancel', cancelCallback)
+    RazorpayCheckout.open(options) */
   }
 }
